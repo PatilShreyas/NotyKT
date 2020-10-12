3c8922249df4a172d9c16361a760c63bd58a630f
Shreyas <shreyaspatilg@gmail.com>
2020-10-12 22:24:10 +0530

    Setup new app submodule: composeapp

--- noty-android/app/composeapp/src/test/java/dev/shreyaspatil/noty/ExampleUnitTest.kt
+++ noty-android/app/composeapp/src/test/java/dev/shreyaspatil/noty/ExampleUnitTest.kt
@@ -14,13 +14,20 @@
  * limitations under the License.
  */
 
-rootProject.name = "Noty"
+package dev.shreyaspatil.noty
 
-include ':core'
-include ':data:remote'
-include ':data:local'
-include ':repository'
-include ':viewmodel'
+import org.junit.Test
 
-include ':app:simpleapp'
-include ':app:composeapp'
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
