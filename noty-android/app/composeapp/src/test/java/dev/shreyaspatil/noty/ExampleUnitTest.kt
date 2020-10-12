d8becf7954795a2164b5307b9185048c7a5cc32c
Shreyas <shreyaspatilg@gmail.com>
2020-10-12 22:45:27 +0530

    Reformat with Ktlint

--- noty-android/app/composeapp/src/test/java/dev/shreyaspatil/noty/ExampleUnitTest.kt
+++ noty-android/app/composeapp/src/test/java/dev/shreyaspatil/noty/ExampleUnitTest.kt
@@ -16,9 +16,8 @@
 
 package dev.shreyaspatil.noty
 
-import org.junit.Test
-
 import org.junit.Assert.*
+import org.junit.Test
 
 /**
  * Example local unit test, which will execute on the development machine (host).
