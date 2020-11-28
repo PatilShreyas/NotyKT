3d8cfbfb4fcd06decdab14ac621da2ebea843fd4
Shreyas <shreyaspatilg@gmail.com>
2020-11-28 17:25:25 +0530

    Use local repository in NotesViewModel and add sync operations

--- noty-android/app/src/main/java/dev/shreyaspatil/noty/view/viewmodel/NotesViewModel.kt
+++ noty-android/app/src/main/java/dev/shreyaspatil/noty/view/viewmodel/NotesViewModel.kt
@@ -17,57 +17,68 @@
 package dev.shreyaspatil.noty.view.viewmodel
 
 import androidx.hilt.lifecycle.ViewModelInject
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import dev.shreyaspatil.noty.core.model.Note
 import dev.shreyaspatil.noty.core.preference.PreferenceManager
 import dev.shreyaspatil.noty.core.repository.NotyNoteRepository
 import dev.shreyaspatil.noty.core.repository.ResponseResult
 import dev.shreyaspatil.noty.core.session.SessionManager
+import dev.shreyaspatil.noty.core.task.NotyTaskManager
+import dev.shreyaspatil.noty.core.task.TaskState
 import dev.shreyaspatil.noty.core.view.ViewState
+import dev.shreyaspatil.noty.di.LocalRepository
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 
 @ExperimentalCoroutinesApi
 class NotesViewModel @ViewModelInject constructor(
-    private val notyNoteRepository: NotyNoteRepository,
+    @LocalRepository private val notyNoteRepository: NotyNoteRepository,
     private val sessionManager: SessionManager,
-    private val preferenceManager: PreferenceManager
+    private val preferenceManager: PreferenceManager,
+    private val notyTaskManager: NotyTaskManager
 ) : ViewModel() {
 
-    private var job: Job? = null
+    private var syncJob: Job? = null
 
-    private val _notesState = MutableLiveData<ViewState<List<Note>>>()
+    private val _syncState = MutableLiveData<ViewState<Unit>>()
+    val syncState: LiveData<ViewState<Unit>> = _syncState
 
-    val notesState: LiveData<ViewState<List<Note>>> = _notesState
+    val notes: LiveData<ViewState<List<Note>>> = notyNoteRepository.getAllNotes()
+        .map { result ->
+            when (result) {
+                is ResponseResult.Success -> ViewState.success(result.data)
+                is ResponseResult.Error -> ViewState.failed(result.message)
+            }
+        }.asLiveData()
 
-    fun getAllNotes() {
-        job?.cancel()
+    fun syncNotes() {
+        syncJob?.cancel()
+        syncJob = viewModelScope.launch {
+            val taskId = notyTaskManager.syncNotes()
 
-        job = viewModelScope.launch {
-            notyNoteRepository.getAllNotes()
-                .onStart { _notesState.value = ViewState.loading() }
-                .collect {
-                    val viewState = when (it) {
-                        is ResponseResult.Success -> ViewState.success(it.data)
-                        is ResponseResult.Error -> ViewState.failed(it.message)
-                    }
-                    _notesState.value = viewState
+            notyTaskManager.observeTask(taskId).collect { taskState ->
+                val viewState = when (taskState) {
+                    TaskState.SCHEDULED -> ViewState.loading()
+                    TaskState.COMPLETED, TaskState.CANCELLED -> ViewState.success(Unit)
+                    TaskState.FAILED -> ViewState.failed("Failed")
                 }
+
+                _syncState.value = viewState
+            }
         }
     }
 
     fun isUserLoggedIn() = sessionManager.getToken() != null
 
     fun clearUserSession() {
-        sessionManager.saveToken(null)
-        viewModelScope.launch {
+        viewModelScope.launch(Dispatchers.IO) {
+            sessionManager.saveToken(null)
+            notyTaskManager.abortAllTasks()
             notyNoteRepository.deleteAllNotes()
         }
     }
