47d4a606d5540c9d963ac0cb9a8411de1c4adefb
Shreyas <shreyaspatilg@gmail.com>
2020-11-28 17:10:03 +0530

    Provide configuration for WorkManager in App entry-point using Hilt

--- noty-android/app/simpleapp/src/main/java/dev/shreyaspatil/noty/simpleapp/NotyApp.kt
+++ noty-android/app/simpleapp/src/main/java/dev/shreyaspatil/noty/simpleapp/NotyApp.kt
@@ -17,11 +17,21 @@
 package dev.shreyaspatil.noty.simpleapp
 
 import android.app.Application
+import android.util.Log
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class NotyApp : Application() {
-    override fun onCreate() {
-        super.onCreate()
-    }
+class NotyApp : Application(), Configuration.Provider {
+
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+
+    override fun getWorkManagerConfiguration() =
+        Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .setMinimumLoggingLevel(Log.DEBUG)
+            .build()
 }
