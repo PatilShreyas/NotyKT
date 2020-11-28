cde620f28aacf7d4c87503e93521b4be7e6a3c8c
Shreyas <shreyaspatilg@gmail.com>
2020-11-28 17:21:04 +0530

    Create DI module: NotyTaskManager

---
+++ noty-android/app/src/main/java/dev/shreyaspatil/noty/di/NotyTaskManagerModule.kt
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2020 Shreyas Patil
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shreyaspatil.noty.di
+
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import dev.shreyaspatil.noty.core.task.NotyTaskManager
+import dev.shreyaspatil.noty.task.NotyTaskManagerImpl
+
+@Module
+@InstallIn(ApplicationComponent::class)
+interface NotyTaskManagerModule {
+
+    @Binds
+    fun notyTaskManager(notyTaskManager: NotyTaskManagerImpl): NotyTaskManager
+}
