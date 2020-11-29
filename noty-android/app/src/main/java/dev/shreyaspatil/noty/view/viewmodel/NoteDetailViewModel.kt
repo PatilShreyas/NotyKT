5358101d58121dcc562a0fbe4fbadf530bb56861
Shreyas <shreyaspatilg@gmail.com>
2020-11-29 12:29:46 +0530

    Revert note loading using `asLiveData()` in NoteDetailViewModel

--- noty-android/app/src/main/java/dev/shreyaspatil/noty/view/viewmodel/NoteDetailViewModel.kt
+++ noty-android/app/src/main/java/dev/shreyaspatil/noty/view/viewmodel/NoteDetailViewModel.kt
@@ -32,6 +32,7 @@ import dev.shreyaspatil.noty.core.view.ViewState
 import dev.shreyaspatil.noty.di.LocalRepository
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 
 @ExperimentalCoroutinesApi
@@ -41,9 +42,17 @@ class NoteDetailViewModel @AssistedInject constructor(
     @Assisted private val noteId: String
 ) : ViewModel() {
 
+    init {
+        viewModelScope.launch {
+            noteRepository.getNoteById(noteId).first()
+                .let { _note.value = it }
+        }
+    }
+
     private var job: Job? = null
 
-    val note: LiveData<Note> = noteRepository.getNoteById(noteId).asLiveData()
+    private val _note = MutableLiveData<Note>()
+    val note: LiveData<Note> = _note
 
     private val _updateNoteState = MutableLiveData<ViewState<Unit>>()
     val updateNoteState: LiveData<ViewState<Unit>> = _updateNoteState
