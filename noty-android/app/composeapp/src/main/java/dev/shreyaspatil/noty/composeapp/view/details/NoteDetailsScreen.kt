feb8addf9c0188e0017a2e8beeeef95a0072f398
Shreyas <shreyaspatilg@gmail.com>
2021-02-02 19:49:58 +0530

    Listen to update/delete changes in note details screen

--- noty-android/app/composeapp/src/main/java/dev/shreyaspatil/noty/composeapp/view/details/NoteDetailsScreen.kt
+++ noty-android/app/composeapp/src/main/java/dev/shreyaspatil/noty/composeapp/view/details/NoteDetailsScreen.kt
@@ -17,32 +17,36 @@
 package dev.shreyaspatil.noty.composeapp.view.details
 
 import android.app.Activity
-import androidx.compose.foundation.ScrollableColumn
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Done
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.AmbientContext
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.navigate
 import dagger.hilt.android.EntryPointAccessors
+import dev.shreyaspatil.noty.composeapp.R
+import dev.shreyaspatil.noty.composeapp.component.action.DeleteAction
+import dev.shreyaspatil.noty.composeapp.component.action.ShareAction
 import dev.shreyaspatil.noty.composeapp.navigation.Screen
+import dev.shreyaspatil.noty.composeapp.utils.toast
 import dev.shreyaspatil.noty.composeapp.view.MainActivity
+import dev.shreyaspatil.noty.core.view.ViewState
+import dev.shreyaspatil.noty.utils.NoteValidator
 import dev.shreyaspatil.noty.view.viewmodel.NoteDetailViewModel
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.InternalCoroutinesApi
@@ -52,12 +56,16 @@ import kotlinx.coroutines.InternalCoroutinesApi
 @Composable
 fun NoteDetailsScreen(
     navController: NavHostController,
-    noteDetailViewModel: NoteDetailViewModel
+    viewModel: NoteDetailViewModel
 ) {
-    val note = noteDetailViewModel.note.collectAsState(initial = null)
 
-    val titleText = mutableStateOf(note.value?.title ?: "")
-    val noteText = mutableStateOf(note.value?.note ?: "")
+    val updateState = viewModel.updateNoteState.collectAsState(initial = null)
+    val deleteState = viewModel.deleteNoteState.collectAsState(initial = null)
+
+    val note = viewModel.note.collectAsState(initial = null)
+
+    val titleText = mutableStateOf(note.value?.title)
+    val noteText = mutableStateOf(note.value?.note)
 
     Scaffold(
         topBar = {
@@ -77,12 +85,20 @@ fun NoteDetailsScreen(
                             navController.navigate(Screen.Notes.route)
                         }
                     ) {
-                        Icon(Icons.Filled.ArrowBack, "Back", tint = MaterialTheme.colors.onPrimary)
+                        Icon(
+                            vectorResource(R.drawable.ic_back),
+                            "Back",
+                            tint = MaterialTheme.colors.onPrimary
+                        )
                     }
                 },
                 backgroundColor = MaterialTheme.colors.onBackground,
-                contentColor = MaterialTheme.colors.secondary,
-                elevation = 0.dp
+                contentColor = MaterialTheme.colors.onPrimary,
+                elevation = 0.dp,
+                actions = {
+                    DeleteAction(onClick = { viewModel.deleteNote() })
+                    ShareAction(onClick = { /* TODO Implement*/ })
+                }
             )
         },
         bodyContent = {
@@ -99,7 +115,7 @@ fun NoteDetailsScreen(
                             fontSize = 24.sp
                         ),
                         backgroundColor = MaterialTheme.colors.background,
-                        value = titleText.value,
+                        value = titleText.value ?: "",
                         onValueChange = { titleText.value = it }
                     )
                 }
@@ -115,13 +131,42 @@ fun NoteDetailsScreen(
                             fontSize = 16.sp
                         ),
                         backgroundColor = MaterialTheme.colors.background,
-                        value = noteText.value,
+                        value = noteText.value ?: "",
                         onValueChange = { noteText.value = it }
                     )
                 }
             }
+        },
+        floatingActionButton = {
+            val noteTitle = titleText.value ?: return@Scaffold
+            val noteContent = noteText.value ?: return@Scaffold
+
+            if (NoteValidator.isValidNote(noteTitle, noteContent)) {
+                ExtendedFloatingActionButton(
+                    text = { Text("Save", color = MaterialTheme.colors.onPrimary) },
+                    icon = {
+                        Icon(
+                            Icons.Filled.Done,
+                            "Save",
+                            tint = MaterialTheme.colors.onPrimary
+                        )
+                    },
+                    onClick = { viewModel.updateNote(noteTitle, noteContent) },
+                    backgroundColor = MaterialTheme.colors.primary
+                )
+            }
         }
     )
+
+    when (val state = updateState.value) {
+        is ViewState.Success -> navController.navigateUp()
+        is ViewState.Failed -> toast(state.message)
+    }
+
+    when (val state = deleteState.value) {
+        is ViewState.Success -> navController.navigateUp()
+        is ViewState.Failed -> toast(state.message)
+    }
 }
 
 @InternalCoroutinesApi
