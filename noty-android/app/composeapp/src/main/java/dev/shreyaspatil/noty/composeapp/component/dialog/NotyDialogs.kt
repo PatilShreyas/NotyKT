387fa93391cc1c96358e099e042188fc282ae516
Shreyas <shreyaspatilg@gmail.com>
2021-02-06 23:06:00 +0530

    Provide button for dismiss dialog for failure dialog

--- noty-android/app/composeapp/src/main/java/dev/shreyaspatil/noty/composeapp/component/dialog/NotyDialogs.kt
+++ noty-android/app/composeapp/src/main/java/dev/shreyaspatil/noty/composeapp/component/dialog/NotyDialogs.kt
@@ -16,13 +16,11 @@
 
 package dev.shreyaspatil.noty.composeapp.component.dialog
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -30,29 +28,56 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import dev.shreyaspatil.noty.composeapp.R
+import dev.shreyaspatil.noty.composeapp.component.anim.LottieAnimation
+import dev.shreyaspatil.noty.composeapp.ui.typography
 
 @Composable
 fun LoaderDialog() {
     Dialog(onDismissRequest = {}) {
         Surface(modifier = Modifier.size(128.dp)) {
-            LottieAnimation(resId = R.raw.loading)
+            LottieAnimation(resId = R.raw.loading, modifier = Modifier
+                .padding(16.dp)
+                .size(100.dp))
         }
     }
 }
 
 @Composable
-fun FailureDialog(failureMessage: String) {
-    Dialog(onDismissRequest = {}) {
-        Surface {
-            Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                LottieAnimation(resId = R.raw.failure)
-                Text(
-                    text = failureMessage,
-                    color = Color.Black,
-                    style = MaterialTheme.typography.subtitle1,
-                    fontWeight = FontWeight.Bold,
-                    modifier = Modifier.padding(16.dp)
-                )
+fun FailureDialog(failureMessage: String, onDismissed: () -> Unit = {}) {
+    val isDismissed = remember { mutableStateOf(false) }
+
+    if (!isDismissed.value) {
+        Dialog(onDismissRequest = {}) {
+            Surface {
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    LottieAnimation(
+                        resId = R.raw.failure,
+                        modifier = Modifier
+                            .padding(16.dp)
+                            .size(84.dp)
+                    )
+                    Text(
+                        text = failureMessage,
+                        color = Color.Black,
+                        style = MaterialTheme.typography.subtitle1,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier.padding(16.dp)
+                    )
+
+                    Button(
+                        onClick = {
+                            isDismissed.value = true
+                        },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(80.dp)
+                            .padding(16.dp),
+
+                    ) {
+                        Text(style = typography.subtitle1, color = Color.White, text = "OK")
+                    }
+
+                }
             }
         }
     }
