ac2492e234d7524de321ec30c6daf12f0e204291
Shreyas <shreyaspatilg@gmail.com>
2021-02-06 22:30:38 +0530

    Add Dialogs for loading and failure

---
+++ noty-android/app/composeapp/src/main/java/dev/shreyaspatil/noty/composeapp/component/dialog/NotyDialogs.kt
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2020 Shreyas Patil
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shreyaspatil.noty.composeapp.component.dialog
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import dev.shreyaspatil.noty.composeapp.R
+
+@Composable
+fun LoaderDialog() {
+    Dialog(onDismissRequest = {}) {
+        Surface(modifier = Modifier.size(128.dp)) {
+            LottieAnimation(resId = R.raw.loading)
+        }
+    }
+}
+
+@Composable
+fun FailureDialog(failureMessage: String) {
+    Dialog(onDismissRequest = {}) {
+        Surface {
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                LottieAnimation(resId = R.raw.failure)
+                Text(
+                    text = failureMessage,
+                    color = Color.Black,
+                    style = MaterialTheme.typography.subtitle1,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+        }
+    }
+}
