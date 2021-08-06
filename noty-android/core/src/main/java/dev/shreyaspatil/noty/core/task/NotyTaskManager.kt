30cef591f4120927174064ed145d7a4cb20e4bdd
Shreyas <shreyaspatilg@gmail.com>
2021-08-06 17:37:51 +0530

    Add docs for NotyTaskManager.kt

--- noty-android/core/src/main/java/dev/shreyaspatil/noty/core/task/NotyTaskManager.kt
+++ noty-android/core/src/main/java/dev/shreyaspatil/noty/core/task/NotyTaskManager.kt
@@ -23,10 +23,43 @@ import javax.inject.Singleton
 
 @Singleton
 interface NotyTaskManager {
+    /**
+     * Schedules a task for syncing notes.
+     *
+     * @return Unique work ID
+     */
     fun syncNotes(): UUID
+
+    /**
+     * Schedules a [NotyTask] task
+     *
+     * @return Unique work ID
+     */
     fun scheduleTask(notyTask: NotyTask): UUID
+
+    /**
+     * Retrieves the state of a task
+     *
+     * @param taskId Unique work ID
+     * @return Nullable (in case task not exists) task state
+     */
     fun getTaskState(taskId: UUID): TaskState?
+
+    /**
+     * Returns Flowable task state of a specific task
+     *
+     * @param taskId Unique work ID
+     * @return Flow of task state
+     */
     fun observeTask(taskId: UUID): Flow<TaskState>
+
+    /**
+     * Aborts/Stops all scheduled (ongoing) tasks
+     */
     fun abortAllTasks()
+
+    /**
+     * Generates task ID from note ID
+     */
     fun getTaskIdFromNoteId(noteId: String) = noteId
 }
