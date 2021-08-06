2f0cd0fd06d5b2c3b565e2248293494406b31d48
Shreyas <shreyaspatilg@gmail.com>
2021-08-06 15:53:27 +0530

    Use common note fields in add note screen

--- noty-android/app/composeapp/src/main/java/dev/shreyaspatil/noty/composeapp/ui/screens/AddNotesScreen.kt
+++ noty-android/app/composeapp/src/main/java/dev/shreyaspatil/noty/composeapp/ui/screens/AddNotesScreen.kt
@@ -16,18 +16,21 @@
 
 package dev.shreyaspatil.noty.composeapp.ui.screens
 
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material.ExtendedFloatingActionButton
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
-import androidx.compose.material.TextField
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Done
@@ -38,20 +41,20 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import dev.shreyaspatil.noty.composeapp.R
 import dev.shreyaspatil.noty.composeapp.component.dialog.FailureDialog
 import dev.shreyaspatil.noty.composeapp.component.dialog.LoaderDialog
+import dev.shreyaspatil.noty.composeapp.component.text.NoteField
+import dev.shreyaspatil.noty.composeapp.component.text.NoteTitleField
 import dev.shreyaspatil.noty.core.view.ViewState
 import dev.shreyaspatil.noty.utils.validator.NoteValidator
 import dev.shreyaspatil.noty.view.viewmodel.AddNoteViewModel
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 
+@ExperimentalAnimationApi
 @ExperimentalCoroutinesApi
 @Composable
 fun AddNoteScreen(
@@ -74,7 +77,7 @@ fun AddNoteScreen(
             TopAppBar(
                 title = {
                     Text(
-                        text = "Noty",
+                        text = "Add Note",
                         textAlign = TextAlign.Start,
                         color = MaterialTheme.colors.onPrimary,
                         modifier = Modifier.fillMaxWidth()
@@ -82,7 +85,7 @@ fun AddNoteScreen(
                 },
                 navigationIcon = {
                     IconButton(
-                        modifier = Modifier.padding(12.dp, 0.dp, 0.dp, 0.dp),
+                        modifier = Modifier.padding(4.dp, 0.dp, 0.dp, 0.dp),
                         onClick = {
                             navController.navigateUp()
                         }
@@ -100,39 +103,31 @@ fun AddNoteScreen(
             )
         },
         content = {
-            LazyColumn {
-                item {
-                    TextField(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(16.dp, 0.dp, 16.dp, 0.dp)
-                            .background(MaterialTheme.colors.background),
-                        label = { Text(text = "Title") },
-                        textStyle = TextStyle(
-                            color = MaterialTheme.colors.onPrimary,
-                            fontWeight = FontWeight.Bold,
-                            fontSize = 24.sp
-                        ),
-                        value = titleText.value,
-                        onValueChange = { titleText.value = it }
-                    )
-                }
-                item {
-                    TextField(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .fillMaxHeight()
-                            .padding(16.dp, 0.dp, 16.dp, 0.dp)
-                            .background(MaterialTheme.colors.background),
-                        label = { Text(text = "Write something...") },
-                        textStyle = TextStyle(
-                            color = MaterialTheme.colors.onPrimary,
-                            fontSize = 16.sp
-                        ),
-                        value = noteText.value,
-                        onValueChange = { noteText.value = it }
-                    )
-                }
+            Column(
+                Modifier.scrollable(
+                    rememberScrollState(),
+                    orientation = Orientation.Vertical
+                )
+            ) {
+
+                NoteTitleField(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp, 0.dp, 16.dp, 0.dp)
+                        .background(MaterialTheme.colors.background),
+                    value = titleText.value,
+                    onTextChange = { titleText.value = it },
+                )
+
+                NoteField(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .fillMaxHeight()
+                        .padding(16.dp, 8.dp, 16.dp, 0.dp)
+                        .background(MaterialTheme.colors.background),
+                    value = noteText.value,
+                    onTextChange = { noteText.value = it }
+                )
             }
         },
         floatingActionButton = {
