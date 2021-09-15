cd22ec62f7a9f466ce0f53af059a702a0a221c62
Shreyas <shreyaspatilg@gmail.com>
2021-09-15 19:24:21 +0530

    Segregate JWT Controller in interface

--- noty-api/application/src/dev/shreyaspatil/noty/api/auth/JWTController.kt
+++ noty-api/application/src/dev/shreyaspatil/noty/api/auth/JWTController.kt
@@ -19,14 +19,26 @@ package dev.shreyaspatil.noty.api.auth
 import com.auth0.jwt.JWT
 import com.auth0.jwt.JWTVerifier
 import com.auth0.jwt.algorithms.Algorithm
+import dev.shreyaspatil.noty.api.di.module.SecretKey
+import javax.inject.Inject
+
+interface JWTController {
+
+    val verifier: JWTVerifier
+
+    /**
+     * Generates JWT Token from [data]
+     */
+    fun sign(data: String): String
+}
 
 /**
  * Simple implementation for providing JWT Authentication mechanism.
  * Use [sign] method to generate token.
  */
-open class NotyJWT private constructor(secret: String) {
+class NotyJWTController @Inject constructor(@SecretKey secret: String) : JWTController {
     private val algorithm = Algorithm.HMAC256(secret)
-    val verifier: JWTVerifier = JWT
+    override val verifier: JWTVerifier = JWT
         .require(algorithm)
         .withIssuer(ISSUER)
         .withAudience(AUDIENCE)
@@ -35,25 +47,14 @@ open class NotyJWT private constructor(secret: String) {
     /**
      * Generates JWT token from [userId].
      */
-    fun sign(userId: String): String = JWT
+    override fun sign(data: String): String = JWT
         .create()
         .withIssuer(ISSUER)
         .withAudience(AUDIENCE)
-        .withClaim(ClAIM, userId)
+        .withClaim(ClAIM, data)
         .sign(algorithm)
 
     companion object {
-        lateinit var instance: NotyJWT
-            private set
-
-        fun initialize(secret: String) {
-            synchronized(this) {
-                if (!this::instance.isInitialized) {
-                    instance = NotyJWT(secret)
-                }
-            }
-        }
-
         private const val ISSUER = "NotyKT-JWT-Issuer"
         private const val AUDIENCE = "https://noty-api.herokuapp.com"
         const val ClAIM = "userId"
