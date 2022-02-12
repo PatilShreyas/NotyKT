84e6db8cad8d128fb07c4422fceba30f1ba267b1
Shreyas <shreyaspatilg@gmail.com>
2022-02-12 18:07:27 +0530

    Refactor: make core implementation at NetworkUtils

--- noty-android/app/src/main/java/dev/shreyaspatil/noty/connectivity/ConnectivityObserverImpl.kt
+++ noty-android/app/src/main/java/dev/shreyaspatil/noty/connectivity/ConnectivityObserverImpl.kt
@@ -17,16 +17,12 @@
 package dev.shreyaspatil.noty.connectivity
 
 import android.net.ConnectivityManager
-import android.net.Network
-import android.net.NetworkCapabilities
-import android.net.NetworkRequest
-import android.util.Log
 import dev.shreyaspatil.noty.core.connectivity.ConnectionState
 import dev.shreyaspatil.noty.core.connectivity.ConnectivityObserver
+import dev.shreyaspatil.noty.utils.currentConnectivityState
+import dev.shreyaspatil.noty.utils.observeConnectivityAsFlow
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.callbackFlow
 
 @OptIn(ExperimentalCoroutinesApi::class)
 class ConnectivityObserverImpl(
@@ -34,66 +30,8 @@ class ConnectivityObserverImpl(
 ) : ConnectivityObserver {
 
     override val connectionState: Flow<ConnectionState>
-        get() = observeConnectivityAsFlow()
+        get() = connectivityManager.observeConnectivityAsFlow()
 
     override val currentConnectionState: ConnectionState
-        get() = getCurrentConnectivityState()
-
-
-    @ExperimentalCoroutinesApi
-    fun observeConnectivityAsFlow() = callbackFlow {
-        // Set current state
-        val currentState = getCurrentConnectivityState()
-        trySend(currentState)
-
-        val callback = NetworkCallback { connectionState -> trySend(connectionState) }
-
-        val networkRequest = NetworkRequest.Builder()
-            .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
-            .build()
-
-        connectivityManager.registerNetworkCallback(networkRequest, callback)
-
-        awaitClose {
-            connectivityManager.unregisterNetworkCallback(callback)
-        }
-    }
-
-    private fun getCurrentConnectivityState(): ConnectionState {
-        // Retrieve current status of connectivity
-        val connected = connectivityManager.allNetworks.any { network ->
-            connectivityManager.getNetworkCapabilities(network)
-                ?.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
-                ?: false
-        }
-
-        return if (connected) ConnectionState.Available else ConnectionState.Unavailable
-    }
-
-
-    @Suppress("FunctionName")
-    private fun NetworkCallback(
-        callback: (ConnectionState) -> Unit
-    ): ConnectivityManager.NetworkCallback {
-        return object : ConnectivityManager.NetworkCallback() {
-            override fun onAvailable(network: Network) {
-                Log.d(TAG, "NetworkCallback: onAvailable")
-                callback(ConnectionState.Available)
-            }
-
-            override fun onLost(network: Network) {
-                Log.d(TAG, "NetworkCallback: onLost")
-                callback(ConnectionState.Unavailable)
-            }
-
-            override fun onUnavailable() {
-                Log.d(TAG, "NetworkCallback: onUnavailable")
-                callback(ConnectionState.Unavailable)
-            }
-        }
-    }
-
-    companion object {
-        private const val TAG = "ConnectivityObserver"
-    }
+        get() = connectivityManager.currentConnectivityState
 }
