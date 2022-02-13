350d76fcefabea09a21cc2fe54ff06d952391255
Shreyas <shreyaspatilg@gmail.com>
2022-02-13 14:40:03 +0530

    NotyKT-397 | Add common utility for showing preview

---
+++ noty-android/app/composeapp/src/main/java/dev/shreyaspatil/noty/composeapp/utils/Preview.kt
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2020 Shreyas Patil
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shreyaspatil.noty.composeapp.utils
+
+import androidx.compose.runtime.Composable
+import dev.shreyaspatil.noty.composeapp.ui.theme.NotyTheme
+
+@Composable
+fun NotyPreview(content: @Composable () -> Unit) {
+    NotyTheme(content = content)
+}
