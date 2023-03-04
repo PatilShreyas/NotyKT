58ecacf535ab2c61e466e17d618df1f02bab7019
Shreyas <shreyaspatilg@gmail.com>
2023-03-04 16:09:16 +0530

    Fix NotesScreenTest: use different nodes for click test

--- noty-android/app/composeapp/src/androidTest/java/dev/shreyaspatil/noty/composeapp/ui/screens/NotesScreenTest.kt
+++ noty-android/app/composeapp/src/androidTest/java/dev/shreyaspatil/noty/composeapp/ui/screens/NotesScreenTest.kt
@@ -196,13 +196,13 @@ class NotesScreenTest : NotyScreenTest() {
         setNotyContent { NotesScreen(onNavigateToNoteDetail = { navigateToNoteId = it }) }
         registerIdlingResource(prefillNotes())
 
-        onNodeWithText("Lorem Ipsum 1", useUnmergedTree = true).performClick()
-        waitForIdle()
-        assertEquals("1", navigateToNoteId)
-
-        onNodeWithText("Hello World 2", useUnmergedTree = true).performClick()
+        onNodeWithText("Lorem Ipsum 2", useUnmergedTree = true).performClick()
         waitForIdle()
         assertEquals("2", navigateToNoteId)
+
+        onNodeWithText("Hello World 3", useUnmergedTree = true).performClick()
+        waitForIdle()
+        assertEquals("3", navigateToNoteId)
     }
 
     @Composable
