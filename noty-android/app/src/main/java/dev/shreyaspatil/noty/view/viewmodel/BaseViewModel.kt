c302175706cd3cea2ad5117f8361df9f25942634
Shreyas <shreyaspatilg@gmail.com>
2023-03-22 16:05:51 +0530

    Refactor BaseViewModel: provide state with abstraction

--- noty-android/app/src/main/java/dev/shreyaspatil/noty/view/viewmodel/BaseViewModel.kt
+++ noty-android/app/src/main/java/dev/shreyaspatil/noty/view/viewmodel/BaseViewModel.kt
@@ -18,36 +18,22 @@ package dev.shreyaspatil.noty.view.viewmodel
 
 import androidx.lifecycle.ViewModel
 import dev.shreyaspatil.noty.view.state.State
-import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.updateAndGet
 
 /**
  * Base for all the ViewModels
  */
-abstract class BaseViewModel<STATE : State>(initialState: STATE) : ViewModel() {
-
-    /**
-     * Mutable State of this ViewModel
-     */
-    private val _state = MutableStateFlow(initialState)
-
+abstract class BaseViewModel<STATE : State> : ViewModel() {
     /**
      * State to be exposed to the UI layer
      */
-    val state: StateFlow<STATE> = _state.asStateFlow()
+    abstract val state: StateFlow<STATE>
 
     /**
      * Retrieves the current UI state
      */
     val currentState: STATE get() = state.value
-
-    /**
-     * Updates the state of this ViewModel and returns the new state
-     *
-     * @param update Lambda callback with old state to calculate a new state
-     * @return The updated state
-     */
-    protected fun setState(update: (old: STATE) -> STATE): STATE = _state.updateAndGet(update)
 }
+
+
+
