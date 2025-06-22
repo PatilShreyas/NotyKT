1fb3de88815cbe876668a14edc4fdeac87c79bd5
Shreyas Patil <shreyaspatilg@gmail.com>
2025-06-22 16:43:12 +0530

    Enable dynamic colors

--- noty-android/app/simpleapp/src/main/java/dev/shreyaspatil/noty/simpleapp/NotyApp.kt
+++ noty-android/app/simpleapp/src/main/java/dev/shreyaspatil/noty/simpleapp/NotyApp.kt
@@ -20,6 +20,7 @@ import android.app.Application
 import android.util.Log
 import androidx.hilt.work.HiltWorkerFactory
 import androidx.work.Configuration
+import com.google.android.material.color.DynamicColors
 import dagger.hilt.android.HiltAndroidApp
 import javax.inject.Inject
 
@@ -34,4 +35,9 @@ class NotyApp : Application(), Configuration.Provider {
                 .setWorkerFactory(workerFactory)
                 .setMinimumLoggingLevel(Log.DEBUG)
                 .build()
+
+    override fun onCreate() {
+        super.onCreate()
+        DynamicColors.applyToActivitiesIfAvailable(this)
+    }
 }
